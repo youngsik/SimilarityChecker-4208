ad2f0b5dce4bded38315e4b4c610d87f92bc057b
dean <dean4208@gmail.com>
2025-06-04 16:25:37 +0900

    [feature]CountDifferenceStringLength

--- src/main/java/SimilarityChecker.java
+++ src/main/java/SimilarityChecker.java
@@ -7,10 +7,33 @@ public class SimilarityChecker {
 
     public int guessStrCountMark(String input1, String input2) {
 
+        int longLength;
+        int shortLength;
+        int mark;
         if (input1.length() == input2.length())
         {
-            return 60;
+            mark =  60;
         }
-        return 0;
+        else
+        {
+            longLength = input1.length();
+            shortLength = input2.length();
+
+            if(input2.length() > input1.length())
+            {
+                longLength = input2.length();
+                shortLength = input1.length();
+            }
+
+            double gab = (double)(longLength-shortLength);
+
+            double gabDivideShort = gab / (double) shortLength;
+
+            mark = (int) ((1- gabDivideShort) * 60);
+
+
+        }
+
+        return mark;
     }
 }
--- src/test/java/SimilarityCheckerTest.java
+++ src/test/java/SimilarityCheckerTest.java
@@ -16,6 +16,28 @@ class SimilarityCheckerTest {
         assertEquals(output, expected);
     }
 
+    @Test
+    void CountDifferenceStringLength() {
 
+        SimilarityChecker similarityChecker = new SimilarityChecker();
+        String input1 = "ABCD";
+        String input2 = "EFGHIJ";
+        int expected = 30;
+        int output =similarityChecker.guessStrCountMark(input1,input2);
+
+        assertEquals(output, expected);
+    }
+
+    @Test
+    void CountDifferenceStringLength2() {
+
+        SimilarityChecker similarityChecker = new SimilarityChecker();
+        String input1 = "ABCDEQW";
+        String input2 = "EFG";
+        int expected = -19;
+        int output =similarityChecker.guessStrCountMark(input1,input2);
+
+        assertEquals(output, expected);
+    }
 
 }
