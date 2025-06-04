405a8fb91026ed3c1bdf16b31ef855dc4e7fdb57
dean <dean4208@gmail.com>
2025-06-04 16:16:24 +0900

    [feature]CountSameStringLength

--- src/main/java/SimilarityChecker.java
+++ src/main/java/SimilarityChecker.java
@@ -1,2 +1,16 @@
 public class SimilarityChecker {
+    public int getLength(String input) {
+
+        return input.length();
+    }
+
+
+    public int guessStrCountMark(String input1, String input2) {
+
+        if (input1.length() == input2.length())
+        {
+            return 60;
+        }
+        return 0;
+    }
 }
--- src/test/java/SimilarityCheckerTest.java
+++ src/test/java/SimilarityCheckerTest.java
@@ -1,5 +1,21 @@
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class SimilarityCheckerTest {
+
+    @Test
+    void CountSameStringLength() {
+
+        SimilarityChecker similarityChecker = new SimilarityChecker();
+        String input1 = "ABCD";
+        String input2 = "EFGH";
+        int expected = 60;
+        int output =similarityChecker.guessStrCountMark(input1,input2);
+
+        assertEquals(output, expected);
+    }
+
+
 
 }
